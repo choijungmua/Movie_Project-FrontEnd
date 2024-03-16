7ef149aff8e58ba7fdaee50a6c2ba07bb45bd428
Jungmu <wjdan022@naver.com>
2024-03-16 22:43:08 +0900

    React / useEffect

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,10 +1,32 @@
-import Button from "./button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setCounter((res) => res + 1);
+  };
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  useEffect(() => {
+    console.log("keyword change");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("counter change");
+  }, [counter]);
+  useEffect(() => {
+    console.log("counter and keyword change");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!</h1>
-      <Button text="Button Click" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here... "
+      />
+      <h1>{`버튼 클릿 횟수 : ${counter}`}</h1>
+      <button onClick={onClick}>Button Click</button>
     </div>
   );
 }
