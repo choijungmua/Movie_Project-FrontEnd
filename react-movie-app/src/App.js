401987b6d4f0ad37f91a728eea3a261e3ed9d74c
Jungmu <wjdan022@naver.com>
2024-03-19 00:38:48 +0900

    React / toDos_map

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 function App() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
+  const onChange = (e) => {
+    setToDo(e.target.value);
+  };
   const onSubmit = (e) => {
     e.preventDefault();
     if (toDo === "") {
@@ -14,16 +16,19 @@ function App() {
   console.log(toDos);
   return (
     <div>
-      <h1>My To Dos ToDos({toDos.length})</h1>
+      <h1>My To Dos {toDos.length}</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
           value={toDo}
-          type="text"
-          placeholder="Write your to do... "
+          placeholder="Write your to do..."
         />
         <button>button</button>
       </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
