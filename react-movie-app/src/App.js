e3bf5b01a3667b1de5d8ddf3cbd745b73f26f2e6
Jungmu <wjdan022@naver.com>
2024-03-21 00:50:42 +0900

    React / Router

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
 import Detail from "./routes/Details";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="hello" element={<h1>헬로~</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
